e3597a2b3c532a626fff0478fb8670597e67c816
Akshay JK <akshayjk@zoho.com>
2024-05-25 22:09:26 +0530

    Program to accept user input from command line

--- Index.js
+++ Index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// How to accept user input from commandline
+rl.question("Enter Your Name?\t", (name) => {
+    console.log("You have entered as " + name);
+    rl.question("Enter Your Age?\t", (age) => {
+        const BornYear = new Date().getFullYear() - parseInt(age);
+        console.log("You were born in " + BornYear);
+        rl.close();
+    });
+});

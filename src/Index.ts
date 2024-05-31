46a0cba5b0e0910c5ae25953ca64458159170d76
Akshay JK <akshayjk@zoho.com>
2024-05-31 23:58:16 +0530

    Program to print prime numbers till N

--- src/Index.ts
+++ src/Index.ts
@@ -11,7 +11,17 @@ RL.question('Enter Your Number\t', (num) => {
     console.log("You have entered:\t" + num);
 
     //Calling the function with user input;
-    console.log(num + " is " + (isPrimeNumber(parseInt(num)) ? "a" : "not") + " prime number");
+    const PrimeNumbers = [];
+
+    if (parseInt(num) <= 2) {
+        console.log("The lowest prime number is 2");
+    } else {
+        for (let i = 2; i < parseInt(num); i++) {
+            if (isPrimeNumber(i)) PrimeNumbers.push(i);
+        }
+        console.log(PrimeNumbers, " are the prime numbers between 0 and " + num);
+    }
+
 
     //Giving the control back to CommandLine/Terminal
     RL.close();
@@ -19,9 +29,9 @@ RL.question('Enter Your Number\t', (num) => {
 
 //Function which does the logic
 function isPrimeNumber(num: number): boolean {
-    if(num < 1) return false;
-    for (let i = 1; i < num; i++) {
-        if (i != 1 && num % i == 0) {
+    if (num <= 1) return false;
+    for (let i = 2; i < num; i++) {
+        if (num % i == 0) {
             return false;
         }
     };

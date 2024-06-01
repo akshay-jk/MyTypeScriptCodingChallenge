fb8e125f943cb47a15b6f7202ee610b8455c257e
Akshay JK <akshayjk@zoho.com>
2024-06-01 22:55:52 +0530

    Program to find the factorial of N

--- src/Index.ts
+++ src/Index.ts
@@ -11,29 +11,33 @@ RL.question('Enter Your Number\t', (num) => {
     console.log("You have entered:\t" + num);
 
     //Calling the function with user input;
-    const PrimeNumbers = [];
+    const FactorialValue01: number = findFactorial(parseInt(num));
+    console.log(FactorialValue01 + " is the factorial of " + num + ". (Normal)");
 
-    if (parseInt(num) <= 2) {
-        console.log("The lowest prime number is 2");
-    } else {
-        for (let i = 2; i < parseInt(num); i++) {
-            if (isPrimeNumber(i)) PrimeNumbers.push(i);
-        }
-        console.log(PrimeNumbers, " are the prime numbers between 0 and " + num);
-    }
-
+    //Calling the recursive function with user input;
+    const FactorialValue02: number = findFactorialByRecursion(parseInt(num));
+    console.log(FactorialValue02 + " is the factorial of " + num + ". (Recursion)");
 
     //Giving the control back to CommandLine/Terminal
     RL.close();
 })
 
 //Function which does the logic
-function isPrimeNumber(num: number): boolean {
-    if (num <= 1) return false;
-    for (let i = 2; i < num; i++) {
-        if (num % i == 0) {
-            return false;
-        }
-    };
-    return true;
+function findFactorial(num: number): number {
+    // Factorial of 0 and 1 is 1;
+    if (num == 0 || num == 1) return 1;
+
+    //Factorial calculation of numbers greater than 2
+    let factorialNum = 1;
+    for (let i = num; i > 1; --i) factorialNum *= i;
+    return factorialNum;
+}
+
+//Function which does the logic using recursion
+function findFactorialByRecursion(num: number) {
+    // Factorial of 0 and 1 is 1;
+    if (num == 0 || num == 1) return 1;
+
+    //Factorial calculation of numbers greater than 2
+    return num * findFactorialByRecursion(num - 1);
 }

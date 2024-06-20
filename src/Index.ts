0f2b1b71db82019322731773b42f27d40a461ae0
Akshay JK <akshayjk@zoho.com>
2024-06-20 23:52:48 +0530

    Program to print right angled left-sided triangle

--- src/Index.ts
+++ src/Index.ts
@@ -5,28 +5,15 @@ import * as Readline from 'node:readline';
 
 const RL = Readline.createInterface({ input: process.stdin, output: process.stdout });
 
-//Reading the character from CommandLine/Terminal
 RL.question('Enter the limit\t', (limit) => {
 
-    let FibonacciSeries: Array<Number> = printFibonacciSeries(parseInt(limit));
-    console.log(...FibonacciSeries);
+    printRightTriangle(parseInt(limit));
 
     RL.close();
 });
 
-function printFibonacciSeries(limit: number): Array<Number> {
-    let previousValue: number = 0, presentValue: number = 1;
-    let resultantArray: Array<Number> = [];
-    while (resultantArray.length < limit) {
-        if (resultantArray.length == 0) {
-            resultantArray.push(previousValue, presentValue);
-        } else {
-            let nextValue = previousValue + presentValue;
-            resultantArray.push(nextValue);
-            previousValue = presentValue;
-            presentValue = nextValue;
-        }
+function printRightTriangle(triangleHeight: number): void {
+    for (let i = 0; i < triangleHeight; i++) {
+        console.log("*".repeat(i + 1) + "-".repeat(triangleHeight - (i + 1)));
     }
-
-    return resultantArray;
 }

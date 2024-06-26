c787a65a23fcb28cbaf385e6dd820f59b51c527d
Akshay JK <akshayjk@zoho.com>
2024-06-26 22:15:37 +0530

    Program to print mirror pyramid pattern

--- src/Index.ts
+++ src/Index.ts
@@ -5,16 +5,31 @@ import * as Readline from 'node:readline';
 
 const RL = Readline.createInterface({ input: process.stdin, output: process.stdout });
 
-RL.question('Enter the depth of pyramid\t', (depth) => {
-    printInvertedPyramid(parseInt(depth));
+RL.question('Enter the height of pyramid\t', (height) => {
+    printMirrorPyramid(parseInt(height));
     RL.close();
 });
 
-function printInvertedPyramid(pyramidDepth: number): void {
-    for (let i = 0; i < pyramidDepth; i++) {
+function printMirrorPyramid(pyramidHeight: number): void {
+    
+    for (let i = 0; i < pyramidHeight; i++) {
         let PyramidLine = "";
 
-        let NumberOfHyphens = i, NumberOfStar = pyramidDepth - (i + 1);
+        let NumberOfHyphens = pyramidHeight - (i + 1), NumberOfStar = i;
+
+        PyramidLine += " - ".repeat(NumberOfHyphens);
+        PyramidLine += " * ".repeat(NumberOfStar);
+        PyramidLine += " * ";
+        PyramidLine += " * ".repeat(NumberOfStar);
+        PyramidLine += " - ".repeat(NumberOfHyphens);
+
+        console.log(PyramidLine);
+    }
+
+    for (let i = 0; i < pyramidHeight; i++) {
+        let PyramidLine = "";
+
+        let NumberOfHyphens = i, NumberOfStar = pyramidHeight - (i + 1);
 
         PyramidLine += " - ".repeat(NumberOfHyphens);
         PyramidLine += " * ".repeat(NumberOfStar);

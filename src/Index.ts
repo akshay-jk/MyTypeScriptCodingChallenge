2e80348c18855b66c47faf4917178cd43d95654f
Akshay JK <akshayjk@zoho.com>
2024-06-27 20:58:11 +0530

    Program to print inverted right angled left-sided triangle

--- src/Index.ts
+++ src/Index.ts
@@ -5,38 +5,17 @@ import * as Readline from 'node:readline';
 
 const RL = Readline.createInterface({ input: process.stdin, output: process.stdout });
 
-RL.question('Enter the height of pyramid\t', (height) => {
-    printMirrorPyramid(parseInt(height));
+RL.question('Enter the height of the triangle\t', (height) => {
+
+    const TriangleHeight: number = parseInt(height);
+    PrintInvertedRightAngledLeftSidedTriangle(TriangleHeight);
+
     RL.close();
 });
 
-function printMirrorPyramid(pyramidHeight: number): void {
-    
-    for (let i = 0; i < pyramidHeight; i++) {
-        let PyramidLine = "";
 
-        let NumberOfHyphens = pyramidHeight - (i + 1), NumberOfStar = i;
-
-        PyramidLine += " - ".repeat(NumberOfHyphens);
-        PyramidLine += " * ".repeat(NumberOfStar);
-        PyramidLine += " * ";
-        PyramidLine += " * ".repeat(NumberOfStar);
-        PyramidLine += " - ".repeat(NumberOfHyphens);
-
-        console.log(PyramidLine);
-    }
-
-    for (let i = 0; i < pyramidHeight; i++) {
-        let PyramidLine = "";
-
-        let NumberOfHyphens = i, NumberOfStar = pyramidHeight - (i + 1);
-
-        PyramidLine += " - ".repeat(NumberOfHyphens);
-        PyramidLine += " * ".repeat(NumberOfStar);
-        PyramidLine += " * ";
-        PyramidLine += " * ".repeat(NumberOfStar);
-        PyramidLine += " - ".repeat(NumberOfHyphens);
-
-        console.log(PyramidLine);
+function PrintInvertedRightAngledLeftSidedTriangle(triangleHeight: number): void {
+    for (let i = 0; i < triangleHeight; i++) {
+        console.log(" * ".repeat(triangleHeight - (i)) + " - ".repeat((i)));
     }
 }

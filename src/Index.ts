1e9156600bda52e99dfac2eb2d42287b4c2e8b11
Akshay JK <akshayjk@zoho.com>
2024-07-12 08:01:37 +0530

    Program to demonstrate Bubble Sort

--- src/Index.ts
+++ src/Index.ts
@@ -6,29 +6,22 @@ const RL = Readline.createInterface({ input: process.stdin, output: process.stdo
 
 RL.question("Enter the array elements\t", (input) => {
     const SortedArray: Array<number> = input.split(' ').map(n => parseInt(n));
-    SelectionSort(SortedArray);
+    BubbleSort(SortedArray);
 
     RL.close();
 });
 
-function SelectionSort(Arr: number[]): void {
-    let ArrIndexPointer: number = 0;
-    while (ArrIndexPointer < Arr.length) {
-        let SmallestIntegerIndex: number = ArrIndexPointer;
-
-        for (let i = ArrIndexPointer; i < Arr.length; i++) {
-            if (Arr[i] < Arr[SmallestIntegerIndex]) {
-                SmallestIntegerIndex = i;
+function BubbleSort(ArrAsInput: Array<number>): void {
+    const ArrLen: number = ArrAsInput.length;
+    for (let a of ArrAsInput) {
+        for (let i = 0; i < ArrLen - 1; i++) {
+            if (ArrAsInput[i] > ArrAsInput[i + 1]) {
+                ArrAsInput[i] = ArrAsInput[i] + ArrAsInput[i + 1];
+                ArrAsInput[i + 1] = ArrAsInput[i] - ArrAsInput[i + 1];
+                ArrAsInput[i] = ArrAsInput[i] - ArrAsInput[i + 1];
             }
         }
-        if (SmallestIntegerIndex != ArrIndexPointer) {
-            Arr[ArrIndexPointer] = Arr[ArrIndexPointer] + Arr[SmallestIntegerIndex];
-            Arr[SmallestIntegerIndex] = Arr[ArrIndexPointer] - Arr[SmallestIntegerIndex];
-            Arr[ArrIndexPointer] = Arr[ArrIndexPointer] - Arr[SmallestIntegerIndex];
-        }
-
-        console.log(Arr);
-
-        ArrIndexPointer += 1;
     }
+
+    console.log(ArrAsInput);
 }

4d03a03cd276b30f0e6fc866511bd153c9e7fb25
Akshay JK <akshayjk@zoho.com>
2024-07-13 16:36:23 +0530

    Program to demonstrate Insertion Sort

--- src/Index.ts
+++ src/Index.ts
@@ -5,23 +5,27 @@ import * as Readline from 'node:readline';
 const RL = Readline.createInterface({ input: process.stdin, output: process.stdout });
 
 RL.question("Enter the array elements\t", (input) => {
-    const SortedArray: Array<number> = input.split(' ').map(n => parseInt(n));
-    BubbleSort(SortedArray);
+    const UnsortedArray: Array<number> = input.split(' ').map(n => parseInt(n));
+    InsertionSort(UnsortedArray);
 
     RL.close();
 });
 
-function BubbleSort(ArrAsInput: Array<number>): void {
-    const ArrLen: number = ArrAsInput.length;
-    for (let a of ArrAsInput) {
-        for (let i = 0; i < ArrLen - 1; i++) {
-            if (ArrAsInput[i] > ArrAsInput[i + 1]) {
-                ArrAsInput[i] = ArrAsInput[i] + ArrAsInput[i + 1];
-                ArrAsInput[i + 1] = ArrAsInput[i] - ArrAsInput[i + 1];
-                ArrAsInput[i] = ArrAsInput[i] - ArrAsInput[i + 1];
+function InsertionSort(ArrayToBeSorted: number[]): void {
+    console.log(ArrayToBeSorted);
+    for (let i = 1; i < ArrayToBeSorted.length; i++) {
+        let ArrPointer: number = i;
+        while (ArrPointer > 0) {
+            if (ArrayToBeSorted[ArrPointer - 1] > ArrayToBeSorted[ArrPointer]) {
+                ArrayToBeSorted[ArrPointer - 1] = ArrayToBeSorted[ArrPointer - 1] + ArrayToBeSorted[ArrPointer];
+                ArrayToBeSorted[ArrPointer] = ArrayToBeSorted[ArrPointer - 1] - ArrayToBeSorted[ArrPointer];
+                ArrayToBeSorted[ArrPointer - 1] = ArrayToBeSorted[ArrPointer - 1] - ArrayToBeSorted[ArrPointer];
+                ArrPointer = ArrPointer - 1;
+            } else {
+                break;
             }
         }
-    }
 
-    console.log(ArrAsInput);
+        console.log(ArrayToBeSorted);
+    }
 }

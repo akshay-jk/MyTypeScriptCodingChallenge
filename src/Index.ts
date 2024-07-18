43970f9c02cf18929e53a2f6ee284c4ad73089c6
Akshay JK <akshayjk@zoho.com>
2024-07-18 18:02:29 +0530

    Program to demonstrate Merge Sort

--- src/Index.ts
+++ src/Index.ts
@@ -5,27 +5,63 @@ import * as Readline from 'node:readline';
 const RL = Readline.createInterface({ input: process.stdin, output: process.stdout });
 
 RL.question("Enter the array elements\t", (input) => {
+
     const UnsortedArray: Array<number> = input.split(' ').map(n => parseInt(n));
-    InsertionSort(UnsortedArray);
+    console.log("Array before sorting");
+    console.log(UnsortedArray);
+    console.log();
+
+    MergeSort(UnsortedArray, 0, UnsortedArray.length - 1);
+    console.log("Array after sorting");
+    console.log(UnsortedArray);
 
     RL.close();
 });
 
-function InsertionSort(ArrayToBeSorted: number[]): void {
-    console.log(ArrayToBeSorted);
-    for (let i = 1; i < ArrayToBeSorted.length; i++) {
-        let ArrPointer: number = i;
-        while (ArrPointer > 0) {
-            if (ArrayToBeSorted[ArrPointer - 1] > ArrayToBeSorted[ArrPointer]) {
-                ArrayToBeSorted[ArrPointer - 1] = ArrayToBeSorted[ArrPointer - 1] + ArrayToBeSorted[ArrPointer];
-                ArrayToBeSorted[ArrPointer] = ArrayToBeSorted[ArrPointer - 1] - ArrayToBeSorted[ArrPointer];
-                ArrayToBeSorted[ArrPointer - 1] = ArrayToBeSorted[ArrPointer - 1] - ArrayToBeSorted[ArrPointer];
-                ArrPointer = ArrPointer - 1;
-            } else {
-                break;
-            }
-        }
+function MergeSort(UnsortedArray: number[], StartIndex: number, EndIndex: number): void {
+    if (StartIndex >= EndIndex) return;
 
-        console.log(ArrayToBeSorted);
+    let MidIndex: number = Math.floor((EndIndex + StartIndex) / 2);
+    MergeSort(UnsortedArray, StartIndex, MidIndex);
+    MergeSort(UnsortedArray, MidIndex + 1, EndIndex);
+    Merge(UnsortedArray, StartIndex, MidIndex, EndIndex);
+}
+
+function Merge(Arr: number[], StartIndex: number, MiddleIndex: number, EndIndex: number): void {
+    let FirstArrayLength: number = MiddleIndex - StartIndex + 1;
+    let SecondArrayLength: number = EndIndex - MiddleIndex;
+
+    let i: number = 0, j: number = 0, k: number = StartIndex;
+    let lArr: number[] = [], rArr: number[] = [];
+
+    for (let x = 0; x < FirstArrayLength; x++) {
+        lArr.push(Arr[StartIndex + x]);
+    }
+
+    for (let y = 0; y < SecondArrayLength; y++) {
+        rArr.push(Arr[MiddleIndex + 1 + y]);
+    }
+
+    while (i < FirstArrayLength && j < SecondArrayLength) {
+        if (lArr[i] <= rArr[j]) {
+            Arr[k] = lArr[i];
+            i++;
+        } else {
+            Arr[k] = rArr[j];
+            j++;
+        }
+        k += 1;
+    }
+
+    while (i < FirstArrayLength) {
+        Arr[k] = lArr[i];
+        i++;
+        k += 1
+    }
+
+    while (j < SecondArrayLength) {
+        Arr[k] = rArr[j];
+        j++;
+        k += 1;
     }
 }
